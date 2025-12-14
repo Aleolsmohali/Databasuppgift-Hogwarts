1c479c66266a58889e0b5cd2a6b0fc67769c6996
Aleolsmohali <92396380+Aleolsmohali@users.noreply.github.com>
2025-12-14 17:17:08 +0100

    Update insert_data.sql

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,1 +1,25 @@
+INSERT INTO Students (Id, Name, House) VALUES
+(1, 'Harry Potter', 'Gryffindor'),
+(2, 'Hermione Granger', 'Gryffindor'),
+(3, 'Ron Weasley', 'Gryffindor'),
+(4, 'Draco Malfoy', 'Slytherin'),
+(5, 'Luna Lovegood', 'Ravenclaw'),
+(6, 'Cedric Diggory', 'Hufflepuff');
+
+INSERT INTO Courses (Id, Name, Teacher) VALUES
+(1, 'Defense Against the Dark Arts', 'Remus Lupin'),
+(2, 'Potions', 'Severus Snape'),
+(3, 'Charms', 'Filius Flitwick'),
+(4, 'Herbology', 'Pomona Sprout');
+
+INSERT INTO Enrollments (StudentId, CourseId) VALUES
+(1, 1),
+(1, 3),
+(2, 1),
+(2, 2),
+(2, 3),
+(3, 1),
+(4, 2),
+(5, 3),
+(6, 4);
 
