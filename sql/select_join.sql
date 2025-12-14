ca3626272431c9c41cc1b1d5d80fff0cd3e4cb4f
Aleolsmohali <92396380+Aleolsmohali@users.noreply.github.com>
2025-12-14 20:02:23 +0100

    Update select_join.sql

--- sql/select_join.sql
+++ sql/select_join.sql
@@ -1,12 +1,16 @@
-SELECT s.Name AS Student,
-       c.Name AS Course
+-- Lista elever och deras kursnamn
+SELECT 
+    s.Name AS Student,
+    c.Name AS Course
 FROM Enrollments e
 JOIN Students s ON e.StudentId = s.Id
 JOIN Courses c ON e.CourseId = c.Id;
 
-SELECT c.Name AS Course,
-       c.Teacher,
-       s.Name AS Student
+-- Lista kurser, lärare och elevnamn
+SELECT
+    c.Name AS Course,
+    c.Teacher,
+    s.Name AS Student
 FROM Enrollments e
 JOIN Courses c ON e.CourseId = c.Id
 JOIN Students s ON e.StudentId = s.Id;
