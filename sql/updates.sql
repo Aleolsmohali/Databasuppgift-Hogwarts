6a5bd02cd6586ea7c5101b3144296f1cd6490c2d
Aleolsmohali <92396380+Aleolsmohali@users.noreply.github.com>
2025-12-14 20:01:25 +0100

    Update updates.sql

--- sql/updates.sql
+++ sql/updates.sql
@@ -1,8 +1,11 @@
+-- Byt lärare på kursen Potions (Id = 2)
 UPDATE Courses
 SET Teacher = 'Horace Slughorn'
 WHERE Id = 2;
 
+-- Flytta Luna Lovegood till Gryffindor
 UPDATE Students
 SET House = 'Gryffindor'
 WHERE Name = 'Luna Lovegood';
+
 
