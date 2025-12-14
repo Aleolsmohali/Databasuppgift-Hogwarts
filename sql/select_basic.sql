775d89094fe70dee4046a174bd7c1b5c7cd9ab08
Aleolsmohali <92396380+Aleolsmohali@users.noreply.github.com>
2025-12-14 20:00:09 +0100

    Update select_basic.sql

--- sql/select_basic.sql
+++ sql/select_basic.sql
@@ -1,20 +1,31 @@
-SELECT * FROM Students;
+-- 1. Alla elever
+SELECT * 
+FROM Students;
 
-SELECT * FROM Students
+-- 2. Alla elever från Gryffindor
+SELECT * 
+FROM Students
 WHERE House = 'Gryffindor';
 
-SELECT * FROM Courses
+-- 3. Alla kurser sorterade alfabetiskt
+SELECT * 
+FROM Courses
 ORDER BY Name;
 
-SELECT * FROM Courses
+-- 4. Kurser där lärarens namn börjar på 'S'
+SELECT * 
+FROM Courses
 WHERE Teacher LIKE 'S%';
 
+-- 5. Antal elever per elevhem
 SELECT House, COUNT(*) AS StudentCount
 FROM Students
 GROUP BY House;
 
+-- 6. Antal inskrivningar per kurs
 SELECT CourseId, COUNT(*) AS EnrolledCount
 FROM Enrollments
 GROUP BY CourseId;
+
 
 
