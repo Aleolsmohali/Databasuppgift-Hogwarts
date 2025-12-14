5bd232485381d0deb1703a3d64bcf8e06d3bf91c
Aleolsmohali <92396380+Aleolsmohali@users.noreply.github.com>
2025-12-14 17:17:34 +0100

    Update select_basic.sql

--- sql/select_basic.sql
+++ sql/select_basic.sql
@@ -1,1 +1,16 @@
+SELECT * FROM Students;
+
+SELECT * FROM Students WHERE House = 'Gryffindor';
+
+SELECT * FROM Courses ORDER BY Name;
+
+SELECT * FROM Courses WHERE Teacher LIKE 'S%';
+
+SELECT House, COUNT(*) AS Count
+FROM Students
+GROUP BY House;
+
+SELECT CourseId, COUNT(*) AS Enrolled
+FROM Enrollments
+GROUP BY CourseId;
 
