b01cbee9cfb780c3e090d4155ebf30727da34e33
Aleolsmohali <92396380+Aleolsmohali@users.noreply.github.com>
2025-12-14 17:16:06 +0100

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,21 @@
+CREATE TABLE Students (
+    Id INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL,
+    House TEXT NOT NULL
+);
+
+CREATE TABLE Courses (
+    Id INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL,
+    Teacher TEXT NOT NULL
+);
+
+CREATE TABLE Enrollments (
+    Id INTEGER PRIMARY KEY,
+    StudentId INTEGER NOT NULL,
+    CourseId INTEGER NOT NULL,
+    FOREIGN KEY (StudentId) REFERENCES Students(Id),
+    FOREIGN KEY (CourseId) REFERENCES Courses(Id),
+    UNIQUE (StudentId, CourseId)
+);
 
