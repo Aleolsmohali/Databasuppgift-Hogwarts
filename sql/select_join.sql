6a692dd08a612aa4ebb952c5f9f28f8ac72db925
Aleolsmohali <92396380+Aleolsmohali@users.noreply.github.com>
2025-12-14 17:18:23 +0100

    Update select_join.sql

--- sql/select_join.sql
+++ sql/select_join.sql
@@ -1,1 +1,10 @@
+SELECT Students.Name AS Student, Courses.Name AS Course
+FROM Enrollments
+JOIN Students ON Enrollments.StudentId = Students.Id
+JOIN Courses ON Enrollments.CourseId = Courses.Id;
+
+SELECT Courses.Name AS Course, Courses.Teacher, Students.Name AS Student
+FROM Enrollments
+JOIN Courses ON Enrollments.CourseId = Courses.Id
+JOIN Students ON Enrollments.StudentId = Students.Id;
 
